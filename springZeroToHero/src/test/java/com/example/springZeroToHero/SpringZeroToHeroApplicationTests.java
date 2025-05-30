f2339c9b369f69c677c2187fcb60b724c9a4b51f
Prathameshghorapade <pghorapade17@gmail.com>
2025-05-30 23:41:50 +0530

    start series

---
+++ springZeroToHero/src/test/java/com/example/springZeroToHero/SpringZeroToHeroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springZeroToHero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringZeroToHeroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
